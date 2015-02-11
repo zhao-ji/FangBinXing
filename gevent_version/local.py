e688fa93ddfb54e53138af49a118110072fd0fd7
zhao-ji <iamfrommop@gmail.com>
2015-02-11 14:14:42 +0800

    fixbug: make the local proxy work

--- gevent_version/local.py
+++ gevent_version/local.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf8 -*-
 
+import struct
+
 from gevent import select
 from gevent import socket
 from gevent.server import StreamServer
@@ -11,29 +13,29 @@ REMOTE_ADDR = ("chashuibiao.org", 233)
 
 def handle(local, address):
     # 1. Authorization
-    self.request.recv(262)
-    self.request.send(b"\x05\x00")
+    local.recv(262)
+    local.send(b"\x05\x00")
 
     # 2. Request
-    data = self.rfile.read(4)
+    data = local.recv(4)
     mode = ord(data[1])
     if mode != 1: # if not a TCP/IP stream connection
         reply = b"\x05\x07\x00\x01"  # Command not supported
-        self.request.send(reply)
+        local.send(reply)
         return
 
     addrtype = ord(data[3])
     if addrtype == 1:       # IPv4
         # 4 bytes for IPv4 address
-        addr = socket.inet_ntoa(self.rfile.read(4))
+        addr = socket.inet_ntoa(local.recv(4))
         logbook.info("ipv4 address: {}".format(addr))
     elif addrtype == 3:     # Domain name
         # 1 byte of name length followed by the name for Domain name
-        addr = self.rfile.read(ord(self.rfile.read(1)[0]))
+        addr = local.recv(ord(local.recv(1)))
         logbook.info("domain name is {}".format(addr))
 
     # port number in a network byte order, 2 bytes
-    port = struct.unpack('>H', self.rfile.read(2))[0]
+    port = struct.unpack('>H', local.recv(2))[0]
     logbook.info("request port is {}".format(port))
 
     # 3. Response
@@ -43,7 +45,7 @@ def handle(local, address):
         struct.pack(">H", 65535),
         )
     reply = b"{}{}".format(reply_prefix, reply_suffix)
-    self.request.send(reply)
+    local.send(reply)
 
     # 4. Connect
     remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
